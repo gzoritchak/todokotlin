2d4fd4f3d4f7ab4b61cc0fc1d06905e999578ceb
gzoritchak <g.zoritchak@gmail.com>
2019-06-21 17:14:13 +0200

    Redux: add middleware and partial listener.

--- js/src/main/kotlin/io/data2viz/todo/fwk/Redux.kt
+++ js/src/main/kotlin/io/data2viz/todo/fwk/Redux.kt
@@ -5,7 +5,9 @@ interface StateListener<in S> {
 	fun applyState(state: S)
 }
 
-abstract class Store<State, in Action>(initialState: State) {
+abstract class Store<State, Action>(
+	initialState: State,
+	val middlewares:List<Middleware<State, Action>> = listOf()) {
 
 	abstract fun reducer(state: State, action: Action): State
 
@@ -16,7 +18,8 @@ abstract class Store<State, in Action>(initialState: State) {
 		get() = currentState
 
 	fun dispatch(action: Action) {
-		currentState = reducer(currentState, action)
+		val newAction = applyMiddleWare(action)
+		currentState = reducer(currentState, newAction)
 		listeners.forEach { listener ->
 			listener.applyState(state)
 		}
@@ -25,5 +28,73 @@ abstract class Store<State, in Action>(initialState: State) {
 	fun subscribe(listener: StateListener<State>): () -> Unit {
 		listeners.add(listener)
 		return { listeners.remove(listener) }
+	}
+
+	private fun applyMiddleWare(action: Action): Action {
+		val chain = createNext(0)
+		return chain.next(this, action)
+	}
+
+	private fun createNext(index: Int):Next<State, Action> {
+		if (index == middlewares.size)
+			return EndOfChain()
+		return NextMiddleware(middlewares[index], createNext(index + 1))
+	}
+}
+
+
+interface Middleware<State, Action> {
+	fun applyMiddleware(store: Store<State,Action>, action: Action, next: Next<State,Action>): Action
+}
+
+interface Next<State, Action> {
+	fun next(store: Store<State,Action>, action: Action): Action
+}
+
+class NextMiddleware<State, Action>(
+	val middleware: Middleware<State, Action>,
+	val next: Next<State, Action>): Next<State, Action> {
+
+	override fun next(store: Store<State, Action>, action: Action): Action {
+		return middleware.applyMiddleware(store, action, next)
+	}
+}
+
+class EndOfChain<State,Action>: Next<State, Action> {
+
+	override fun next(store: Store<State, Action>, action: Action): Action {
+		return action
+	}
+}
+
+
+/**
+ * A [Subscriber] that notifies the [PartialSubscriber] if the [SubState] sub-state changed.
+ */
+class PartialSubscriber<in State, SubState>(private val subStateChangeListener: SubStateChangeListener<State, SubState>)
+	: StateListener<State> {
+
+	private var previousValue: SubState? = null
+
+
+	override fun applyState(state: State) {
+		val subState = subStateChangeListener.getSubState(state)
+		if (previousValue == null || subState != previousValue) {
+			subStateChangeListener.onSubStateChanged(subState)
+			previousValue = subState
+		}
+	}
+
+	interface SubStateChangeListener<in State, SubState> {
+
+		/**
+		 * Specify the sub-state you want to listen to.
+		 */
+		fun getSubState(state: State): SubState
+
+		/**
+		 * @param <T> The new sub-state
+		 */
+		fun onSubStateChanged(subState: SubState)
 	}
 }
